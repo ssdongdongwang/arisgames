be895f846172dbf3e2f483fbbae7c754b67f1614
Phildo211 <Phildo211@gmail.com>
2013-03-25 16:40:21 +0000

    merged trunk, added base testing

---
+++ ARISLogicTests/ARISLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  ARISLogicTests.h
+//  ARISLogicTests
+//
+//  Created by Phil Dougherty on 3/25/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ARISLogicTests : SenTestCase
+
+@end
---
+++ ARISLogicTests/ARISLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  ARISLogicTests.m
+//  ARISLogicTests
+//
+//  Created by Phil Dougherty on 3/25/13.
+//
+//
+
+#import "ARISLogicTests.h"
+
+@implementation ARISLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ARISLogicTests");
+}
+
+@end
